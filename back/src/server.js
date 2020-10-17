a2af2c1d64616f5528fb32ee8bc37201191674d9
Pedrok99 <Pedrohlima20@gmail.com>
2020-10-17 15:02:19 -0300

    Adicionando um Hello World

--- back/src/server.js
+++ back/src/server.js
@@ -1,3 +1,11 @@
-/**
- * Code
- */
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Listening to http://localhost:${port}`)
+})
