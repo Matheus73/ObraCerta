6e268aaecfc3e4dedcde6860809fb9efe5696f96
igorq937 <igorq937@gmail.com>
2020-11-06 13:48:21 -0300

    Criação do estilo base

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+
+import ResetStyle from './styles/reset';
 import GlobalStyle from './styles/global';
 
 import Routes from './routes'
 
 function App() {
     return <>
-    <BrowserRouter>
-  <Routes />
-  </BrowserRouter>
-  <GlobalStyle/>
-</>
+      <ResetStyle/>
+      <GlobalStyle/>
+      <BrowserRouter>
+      <Routes />
+      </BrowserRouter>
+    </>
 }
 
 export default App;
