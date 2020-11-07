97131eab604874b6d23ba7aa12fd9fc591fe8cac
Pedrok99 <Pedrohlima20@gmail.com>
2020-11-07 18:01:03 -0300

    Configurações iniciais do knex

---
+++ back/knexfile.js
@@ -0,0 +1,56 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_DATABASE
+    }, 
+    migrations : {
+      tableName :'knex_migrations',
+      directory : `${__dirname}/src/database/migrations`
+    },
+    seeds : {
+      directory : `${__dirname}/src/database/seeds` 
+    }
+  }
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
+
