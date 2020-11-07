da0d30dd9bda09ca53e639f765ab81350e3d5ffd
igorq937 <igorq937@gmail.com>
2020-11-07 18:43:30 -0300

    Refatorando Item

--- front/src/components/Item/index.js
+++ front/src/components/Item/index.js
@@ -2,9 +2,6 @@ import styled from 'styled-components';
 
 const Item = styled.a`
     color: ${props => props.white ? "#FFFFFF" : "#010101"};
-    font-size: 22px;
-    font-weight: bold;
-    cursor: pointer;
 `;
 
 export default Item;
--- front/src/styles/global.js
+++ front/src/styles/global.js
@@ -2,30 +2,33 @@ import {createGlobalStyle} from 'styled-components';
 import trianguloBackground from '../assets/triangulobackground.svg';
 
 export default createGlobalStyle`
-*{
-    margin:0;
-    margin-top: 0;
-    border: 0;
-    padding: 0;
-    outline: 0;
-    box-sizing: border-box;
-    overflow-x: hidden;
-}
-#root {
-    max-width: 970px;
-    margin: 0 auto;
-    padding: 40px 20px;
-}
-body {
-    background: #E5E5E5 url(${trianguloBackground}) no-repeat top;
-    background-size: 100%;
-    -webkit-font-smoothing: antialiased;
-}
-body, input, button {
-    font: 16px Roboto, sans-serif;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
+    *{
+        margin:0;
+        border: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+    #root {
+        max-width: 970px;
+        margin: 0 auto;
+        padding: 0px 15px;
+    }
+    body {
+        overflow-x: hidden;
+        background: #E5E5E5 url(${trianguloBackground}) no-repeat top;
+        background-size: 100%;
+        -webkit-font-smoothing: antialiased;
+    }
+    body, a, button, input {
+        font: 22px Roboto, sans-serif;
+    }
+    a, button {
+        cursor: pointer;
+        font-weight: bold;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
 `;
