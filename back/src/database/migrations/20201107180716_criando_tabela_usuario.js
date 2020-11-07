72202b40da8f3f38e5ccb51ab2a459f8fce11e2c
Pedrok99 <Pedrohlima20@gmail.com>
2020-11-07 18:29:50 -0300

    Criando migration de usuario

---
+++ back/src/database/migrations/20201107180716_criando_tabela_usuario.js
@@ -0,0 +1,17 @@
+const knex = require('..');
+
+exports.up = function(knex) {
+  
+  return knex.schema.createTable('usuario', function(table){
+    table.increments('idUsuario').primary();
+    table.string('email', 50).unique().notNullable();
+    table.string('nomeCompleto', 100).notNullable();
+    table.string('telefone', 15).unique().notNullable(); 
+    table.timestamp('criadoEm').defaultTo(knex.fn.now());  
+  })
+  
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('usuario');
+};
