aaa748213a10b60810ae31bb6956cadd7e5e05de
Pedrok99 <Pedrohlima20@gmail.com>
2020-11-07 23:01:04 -0300

    Criando migration da tabela comentario

---
+++ back/src/database/migrations/20201107225245_criando_tabela_comentario.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+  
+  return knex.schema.createTable('comentario', function(table){
+    table.increments('idComentario').primary();
+    table.string('conteudo', 300).notNullable();
+    table.timestamp('comentarioCriadoEm').defaultTo(knex.fn.now());
+    table.integer('idPublicacao');
+    table.integer('idUsuario'); 
+    table.foreign('idPublicacao').references('idPublicacao').inTable('publicacao').
+    onUpdate('CASCADE').onDelete('CASCADE');
+    table.foreign('idUsuario').references('idUsuario').inTable('usuario').
+    onUpdate('CASCADE').onDelete('CASCADE'); 
+  })
+  
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('comentario');
+};
