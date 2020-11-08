5515ace68d24485f03d56af458a6038cc67c2811
Pedrok99 <Pedrohlima20@gmail.com>
2020-11-07 22:48:39 -0300

    Criando migration da tabela imagem e sua seed

---
+++ back/src/database/migrations/20201107220446_criando_tabela_imagem.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  
+  return knex.schema.createTable('imagem', function(table){
+    
+    table.string('nomeImagem')
+    table.integer('idPublicacao')
+    table.foreign('idPublicacao').references('idPublicacao').inTable('publicacao').
+    onUpdate('CASCADE').onDelete('CASCADE');  
+  })
+  
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('imagem');
+};
