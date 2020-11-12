c101710762b1d1f4c60b44eba322ea252919d8e3
igorq937 <igorq937@gmail.com>
2020-11-12 11:53:14 -0300

    Implementado a página principal

--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
+
 import PageDefault from '../../components/PageDefault';
 import Input from '../../components/Input';
+import Presentation from '../../components/Presentation';
+import imgPresetation from '../../assets/imgPresetation.png'
+
 
 
 function Home(){
@@ -8,9 +12,14 @@ function Home(){
         <PageDefault>
             <Input class="Pesquisa" name="Pesquisa" type="text" placeholder="Procurar profissional"/>
 
+            <Presentation
+                width="400px"
+                src={imgPresetation}
+            >
+            <div>Os melhores profissionais para a sua obra!</div>
+            </Presentation>
         </PageDefault>
-        
-            );
+    );
 }
 
 export default Home;
