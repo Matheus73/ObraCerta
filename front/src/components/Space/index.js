4ced1cd830c948d9b3809beae0dafcfa05ba2dc4
igorq937 <igorq937@gmail.com>
2020-11-15 13:30:44 -0300

    Pequenas melhorias para Space

--- front/src/components/Space/index.js
+++ front/src/components/Space/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Space = styled.div`
+    visibility: hidden;
     margin-top: ${props => props.size || "25px"};
     @media screen and (max-width: 900px){
         margin-top: ${props => props.sizeMobile || props.size};
