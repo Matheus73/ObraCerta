ddd5ee62532da2fc6359ec42262a70bbb1ec1ece
igorq937 <igorq937@gmail.com>
2020-11-16 20:17:32 -0300

    Correções na página de login

--- front/src/pages/Login/index.js
+++ front/src/pages/Login/index.js
@@ -2,47 +2,32 @@ import React from 'react';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import Footer from '../../components/Footer';
-import GlobalStyle from './styles';
 import Item from '../../components/Item';
+import Space from '../../components/Space';
 
 function Login(){
 return (
     <>
-        <GlobalStyle/>
         <div id="content">
-            <h1>Login</h1>
-            <p>Logue para encontrar os melhores profissionais para sua obra ou para divulgar o seu trabalho!</p>
-            <form>
-
-                <div>
-                        <br/>
-                        <label for = "email">Email:</label>
-                    <Input type = "text" id = "email" name="email" placeholder = "Digite seu email" />
-                </div>
-
-                <div>
-                        <br/>
-                        <label for = "password">Senha:</label>
-                    <Input id = "password" type = "password" name="password" placeholder = "Digite sua senha" />
-                </div>
-
-
-                <div>
-                    <br/>
-                    <Item>Esqueci minha senha</Item>
-                    
-                    
-                </div>
-
-                <div>
-                        <br/>
-                    <Button>Enviar</Button>
-                </div>
-
-            </form>
+        <h1>Login</h1>
+        <p>Logue para encontrar os melhores profissionais para sua obra ou para divulgar o seu trabalho!</p>
+        <form>                
+            <br/>
+            <label>Email:
+                <Input type = "text" id = "email" name="email" placeholder = "Digite seu email" />             
+            </label>
+            
+            <br/>
+            <label>Senha:
+                <Input type = "password" name="password" placeholder = "Digite sua senha" />
+            </label> 
+            <br/>
+            <Item href="#">Esqueci minha senha</Item>                              
+            <Space/>
+            <Button>Enviar</Button>
+        </form>
         </div>
-        <Footer color="#F0CA45"/>
-
+        <Footer orange/>
     </>
     );
 }
--- front/src/pages/Login/styles.js
+++ front/src/pages/Login/styles.js
@@ -1,13 +1,10 @@
-// import styled from 'styled-components';
 import {createGlobalStyle} from 'styled-components';
-// import Footer from '../../components/Footer';
 
 export default createGlobalStyle`
     
     body{
         background: #14D0AE url(none);
     }
-
     
 `;
 
