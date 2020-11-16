4ddbfe1193c35f65206267bb391bf3fcac3a7e9a
igorq937 <igorq937@gmail.com>
2020-11-16 20:20:12 -0300

    Melhorando o button

--- front/src/components/Button/index.js
+++ front/src/components/Button/index.js
@@ -16,9 +16,16 @@ const Button = styled(Item).attrs({as:"button"})`
     box-shadow: 0px 10px 4px rgba(0, 0, 0, 0.25);
     margin: 0px 0px 14px 4px;
 
+    &:hover{
+        background: #FfCf4f;
+    }
+
     ${props => props.primary && css`
         ${otherButtons};
         background: #14D0AE;
+        &:hover{
+            background: #1fdfaf;
+        }
     `};
 
     ${props => props.second && css`
