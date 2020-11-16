af9a73b9e113c6a1e064ae3169cf22a6d7a65978
Jhones <tavaresjhones14@gmail.com>
2020-11-16 18:44:42 -0300

    Finalizando o middlewares

--- back/src/routes.js
+++ back/src/routes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const UserController = require('./controllers/UserController')
 const testController = require('./controllers/testTempController.js');//temp
 const loginController = require('./controllers/loginController');
+const authServices = require('./services/authServices');
 
 // Rotas para a pagina inicial da aplicação
 router.get('/', (req, res) => res.send("hello"));
-router.get('/list', testController.listUsers);//temp
+router.get('/list', authServices.middlewares,testController.listUsers);//temp
 
 // Rotas de cadastro e login de usuário
 router.get('/registrar', (req, res) => res.send("Registrar"));
@@ -14,5 +15,6 @@ router.post('/registrar', UserController.store);
 
 router.get('/login', (req, res) => res.send('Logar'));
 router.post('/login', loginController.login);
+
 
 module.exports = router
