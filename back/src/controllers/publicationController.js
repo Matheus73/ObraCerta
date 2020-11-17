f84fac8cb4ccd620c497bfdf38f6a0130bec9962
Samuel Nogueira <samuelnbacelar@hotmail.com>
2020-11-17 10:48:44 -0300

    Criado o publicationController.js

---
+++ back/src/controllers/publicationController.js
@@ -0,0 +1,12 @@
+const yup = require('yup');
+const knex = require('../database/index.js');
+
+class publicationController {
+  async store(req, res){
+    
+  }
+
+}
+
+
+module.exports = new publicationController();
