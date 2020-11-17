ca832158240ae581c48868895e693e9a895ffd87
Samuel Nogueira <samuelnbacelar@hotmail.com>
2020-11-17 14:35:46 -0300

    Metodos store e list de publicationController criados e implementados

--- back/src/controllers/publicationController.js
+++ back/src/controllers/publicationController.js
@@ -1,9 +1,27 @@
 const yup = require('yup');
+const { where } = require('../database/index.js');
 const knex = require('../database/index.js');
+const auth = require('../services/authServices');
 
 class publicationController {
   async store(req, res){
+
+    // necessita de confirmar se o usuário está autenticado
+
+    const dados = auth.decodeToken(req.headers.authorization);
     
+    await knex('publicacao').insert(req.body);
+
+    return res.send({ res: 'publicação criada' });
+  }
+
+  async list(req, res){
+    
+    const user_publications = await knex.select('*')
+    .from('publicacao')
+    .where({ idUsuario: req.body.idUsuario });
+
+    return res.json(user_publications);
   }
 
 }
--- back/src/routes.js
+++ back/src/routes.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const router = express.Router();
-const UserController = require('./controllers/UserController')
+
+const UserController = require('./controllers/UserController');
 const testController = require('./controllers/testTempController.js');//temp
 const loginController = require('./controllers/loginController');
+const publicationController = require('./controllers/publicationController');
+
 const authServices = require('./services/authServices');
 
 // Rotas para a pagina inicial da aplicação
 router.get('/', (req, res) => res.send("hello"));
-router.get('/list', authServices.middlewares,testController.listUsers);//temp
+router.get('/list', authServices.middlewares, testController.listUsers);//temp
 
 // Rotas de cadastro e login de usuário
-router.get('/registrar', (req, res) => res.send("Registrar"));
+router.get('/registrar', UserController.list);
 router.post('/registrar', UserController.store);
 
 router.get('/login', (req, res) => res.send('Logar'));
 router.post('/login', loginController.login);
 
+router.post('/nova_publicacao', authServices.middlewares, publicationController.store);
+router.get('/publicacoes', authServices.middlewares, publicationController.list);
 
 module.exports = router
