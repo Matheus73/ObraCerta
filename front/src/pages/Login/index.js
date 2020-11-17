2e81be7c6b3998118c601a82b3d8942d173ed50e
matheus73 <matheuscoding@gmail.com>
2020-11-17 12:18:01 -0300

    Mudando o Login para OO

--- front/src/pages/Login/index.js
+++ front/src/pages/Login/index.js
@@ -1,35 +1,78 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import Footer from '../../components/Footer';
-import Item from '../../components/Item';
+import GlobalStyle from './styles';
 import Space from '../../components/Space';
 
-function Login(){
-return (
-    <>
-        <div id="content">
-        <h1>Login</h1>
-        <p>Logue para encontrar os melhores profissionais para sua obra ou para divulgar o seu trabalho!</p>
-        <form>                
-            <br/>
-            <label>Email:
-                <Input type="text" name="email" placeholder="Digite seu email" />             
-            </label>
-            
-            <br/>
-            <label>Senha:
-                <Input type="password" name="password" placeholder="Digite sua senha" />
-            </label> 
-            <br/>
-            <Item href="#">Esqueci minha senha</Item>                              
-            <Space/>
-            <Button>Enviar</Button>
-        </form>
-        </div>
-        <Footer orange/>
-    </>
-    );
+class Login extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            email:'',
+            password:'',
+        }
+    }
+
+	handleEmailChange = event => {
+		this.setState({
+			email: event.target.value
+		})
+	}
+
+	handlePasswordChange = event => {
+		this.setState({
+			password: event.target.value
+		})
+	}
+	handleSubmit = event => {
+		event.preventDefault()
+        console.log(this.state)
+	}
+
+	render() {
+        const {email,password} = this.state
+		return (
+            <>
+            <GlobalStyle/>
+            <div id="content">
+            <h1>Login</h1>
+            <p>Entre para encontrar os melhores profissionais para sua obra ou para divulgar o seu trabalho!</p>
+			<form onSubmit={this.handleSubmit}>
+                <Space/>
+					<label>Email:
+                        <Input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder = "Digite seu email" 
+                            value={email}
+                            onChange={this.handleEmailChange}
+                        />
+                    </label>
+                <Space/>
+					<label>Senha:
+                        <Input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder = "Digite sua senha" 
+                            value={password}
+                            onChange={this.handlePasswordChange}
+                        />
+                    </label>
+                <Space/>
+                <div>
+                <Space/>
+                <Button>Enviar</Button>
+                </div>
+			</form>
+            </div>
+            <Footer orange />
+            </>
+		)
+	}
 }
 
-export default Login;
+export default Login
