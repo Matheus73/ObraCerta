df1f40f2adb1a12e8087fa49b015df56922f9d9e
matheus73 <matheuscoding@gmail.com>
2020-11-18 16:15:38 -0300

    Arrumando problema no package-lock e do server.js

--- back/src/server.js
+++ back/src/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", 'Content-Type');
