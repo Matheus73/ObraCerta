1b8abbb5c2234aadbed579966abd4e6fd30a1ed4
Roberto Nóbrega <nobrega.m.roberto@gmail.com>
2020-11-20 18:54:18 -0300

    criado pagina de termos de uso

---
+++ front/src/pages/UseTherms/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+    padding: 30px 50px;
+    margin: 30px;
+    box-shadow: 2px 4px 4px;
+    align-items: center;
+    width: 100%;
+    background: #FFFFFF;
+    border-radius: 30px;
+
+h1, h2, h3{
+text-align: center;
+padding: 15px 0;
+}
+p {
+    text-align: justify;
+    padding: 6px 0;
+}
+ul, ol{
+    padding-left: 40px;
+    text-align: justify;
+}
+
+`;
+
+export default Container;
--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import Cadastro from '../pages/Cadastro';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import UseTherms from '../pages/UseTherms';
 
 function Routes(){
     return(
@@ -11,6 +12,7 @@ function Routes(){
             <Route path="/cadastro" component = {Cadastro}/>
             <Route path="/cadastro" component = {Cadastro}/>
             <Route path="/login" component = {Login}/>
+            <Route path='/termosdeuso' component = {UseTherms}/>
 
         </Switch>
     )
