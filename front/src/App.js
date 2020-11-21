6c36ebebfdc09d0cfa2d54b7f500c321fff915c4
igorq937 <igorq937@gmail.com>
2020-11-20 22:17:41 -0300

    Refatorando as Routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,55 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GlobalStyle from './styles/global';
-
-import Home from './pages/Home';
-import Cadastro from './pages/Cadastro';
-import Login from './pages/Login';
-import axios from 'axios';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      loggedIn: "NOT_LOGGED_IN",
-      user: {}
-    };
-  }
-
-  handleLogin = user => {
-    this.setState({
-      loggedIn: "LOGGED_IN",
-      user: user
-    })
-  }
 
   render() {
     return (
       <>
         <GlobalStyle />
         <BrowserRouter>
-          <Switch>
-            <Route
-              exact path="/"
-              render={props => (
-                <Home {...props} loggedIn={this.state.loggedIn} />
-              )}
-            />
-            <Route
-              path="/cadastro"
-              render={props => (
-                <Cadastro {...props} handleLogin={this.handleLogin} />
-              )}
-            />
-            <Route
-              path="/login"
-              render={props => (
-                <Login {...props} handleLogin={this.handleLogin} />
-              )}
-            />
-          </Switch>
+        <Routes/>
         </BrowserRouter>
       </>
     );
