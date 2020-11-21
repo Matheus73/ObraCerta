468069f1bee302d0e7cab56b726ebd31bd834678
igorq937 <igorq937@gmail.com>
2020-11-20 21:17:42 -0300

    Pequenas correções para Navbar

--- front/src/components/Navbar/index.js
+++ front/src/components/Navbar/index.js
@@ -8,18 +8,19 @@ import Item from '../Item';
 function Navbar(props){
     return (
         <Nav>
-            <Item white href="#" mobileOff>Sobre</Item>
-            <Item white href="/login">Entrar</Item>
-            {props.loggedIn === "NOT_LOGGED" ? 
-                <form action="/cadastro">
-                    <Button second type="submit">CADASTRA-SE</Button>
-                </form>
+            <Item white href="#">Sobre</Item>
+            {props.loggedIn === "NOT_LOGGED_IN" ? 
+                <>
+                    <Item white href="/login">Entrar</Item>
+                    <form action="/cadastro">
+                        <Button second type="submit">CADASTRA-SE</Button>
+                    </form>
+                </>
                 :
-                <form action="/#">
+                <form action="#">
                     <Button second type="submit">Meu perfil</Button>
                 </form>
             }
-            
         </Nav>
     );
 }
