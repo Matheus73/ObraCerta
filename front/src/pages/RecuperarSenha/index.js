fb280fb60c77c5f14dc243e712dd6484dd25f7b0
Roberto Nóbrega <nobrega.m.roberto@gmail.com>
2020-11-22 18:54:21 -0300

    melhorando visibilidade do texto

--- front/src/pages/RecuperarSenha/index.js
+++ front/src/pages/RecuperarSenha/index.js
@@ -6,14 +6,14 @@ import Container from '../TermosDeUsuario/styles';
 function RecoverAccount(props) {
     return (
         <PageDefault loggedIn={props.loggedIn}>
-                <h2>RECUPERAR CONTA</h2>
             <Container>
+                <h2>RECUPERAR CONTA</h2>
 
-                <p>Em caso de esquecimento de senha entre em contato conosco pelo e-mail obracerta@gmail.com</p>
+                <p>Em caso de esquecimento de senha entre em contato conosco pelo e-mail <b>obracerta@gmail.com</b></p>
 
-                <p>Com o assunto: RECUPERAR SENHA</p>
+                <p>Com o assunto: <b>RECUPERAR SENHA</b></p>
 
-                <p>No corpo do e-mail o seu e-mail cadastrado no nosso site.</p>
+                <p>No corpo do e-mail o <b>seu e-mail cadastrado</b> no nosso site.</p>
 
             </Container>
         </PageDefault>
