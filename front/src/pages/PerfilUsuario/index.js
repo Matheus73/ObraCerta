eb86b8c8689bf86159b0109f490a3eea0a25760b
igorq937 <igorq937@gmail.com>
2020-11-26 14:39:19 -0300

    Implementado o ProfileCard

--- front/src/pages/PerfilUsuario/index.js
+++ front/src/pages/PerfilUsuario/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-// import PageDefault from '../../pages/PageDefault';
 import Footer from '../../components/Footer';
 import Card from '../../components/Card';
 import Space from '../../components/Space';
+import ProfileCard from '../../components/ProfileCard';
+import CardGroup from '../../components/CardGroup';
+import imgProfileDefault from '../../assets/profileDefault.png';
 
 class PerfilUsuario extends Component {
 
@@ -10,13 +12,28 @@ class PerfilUsuario extends Component {
         return (
             <>
                 <main>
-                    <h1>PerfilUsuario</h1>
-                    <br/>
-                    <h1>Olá {sessionStorage.getItem("name")}</h1>
-                    <br/>
-                    <Card horizontal width="100%">Email: {sessionStorage.getItem("email")}</Card>
                     <Space/>
-                    <Card horizontal width="100%">Telefone: {sessionStorage.getItem("telefone")}</Card>
+                    <h2>{localStorage.getItem("name")}</h2>
+                    <ProfileCard
+                        src={imgProfileDefault}
+                        title="Descrição:"
+                        description={localStorage.getItem("descricao") || "Sem descrição"}
+                    />
+                    <Space size="10px"/>
+                    <CardGroup>
+                        <Card>
+                            <p><strong>Categoria:</strong><br/>
+                            {localStorage.getItem("categoria") || "Não definido"}</p>
+                        </Card>
+                        <Card>
+                            <p><strong>Telefone:</strong><br/>
+                            {localStorage.getItem("telefone") || "Não definido"}</p>
+                        </Card>
+                        <Card>
+                            <p><strong>Região:</strong><br/>
+                            {localStorage.getItem("regiao") || "Não definido"}</p>
+                        </Card>
+                    </CardGroup>
                 </main>
                 <Footer/>
             </>
