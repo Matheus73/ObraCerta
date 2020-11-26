be4e45f49c4dfb5ef976428e3061ebc244b501b7
Pedrok99 <Pedrohlima20@gmail.com>
2020-11-26 14:12:24 -0300

    Alterações rapidas

--- back/src/database/seeds/001_usuarios.js
+++ back/src/database/seeds/001_usuarios.js
@@ -6,21 +6,23 @@ exports.seed = function(knex) {
       // Inserts seed entries    
 
       const users = []
-      const categorias = ['Serralheiro', 'Pintor', 'Eletricista', 'Marceneiro', 'Encanador', 'Pedreiro', null];
+      const categorias = ['Serralheiro', 'Pintor', 'Eletricista', 'Marceneiro', 'Encanador', 'Pedreiro', null, null];
       const resps = ['Bolacha', 'Biscoito', null];
       let user ;
 
-      for (let index = 1; index < 500; index++) {
+      for (let index = 1; index <= 500; index++) {
         user = {
           email:'user' + index + '@mail.com',
           nomeCompleto:'Test User',
           hashSenha: bcrypt.hashSync('123456', 8),
           telefone:'9999999999',
           categoria: categorias[Math.floor(Math.random()*categorias.length)],
-          imagemPerfil:'fakeimage.jpg',
+          imagemPerfil:'fakePerfilImage.jpg',
           descricao:'trabalho desde 17 anos na area',
           respDeSeguranca: resps[Math.floor(Math.random()*resps.length)]
         }
+
+        users.push(user)
         
       }
       console.log(users)
