8b357dbf32294d49de5d8533ff2735026aa14b24
Roberto Nóbrega <nobrega.m.roberto@gmail.com>
2020-11-27 13:03:35 -0300

    cards inseridos na Sobre, sobre completa

--- front/src/pages/Sobre/index.js
+++ front/src/pages/Sobre/index.js
@@ -3,6 +3,16 @@ import krimps from '../../assets/krimps.svg';
 import PageDefault from '../PageDefault';
 import Container from './styles';
 import Presentation from '../../components/Presentation';
+import Space from '../../components/Space';
+import Card from '../../components/Card';
+import CardGroup from '../../components/CardGroup';
+import Ped from '../../assets/VerticalCards/SobreCards/Ped.svg';
+import Sam from '../../assets/VerticalCards/SobreCards/Sam.svg';
+import Rob from '../../assets/VerticalCards/SobreCards/Rob.svg';
+import Mat from '../../assets/VerticalCards/SobreCards/Mat.svg';
+import Ig from '../../assets/VerticalCards/SobreCards/Ig.svg';
+import Kess from '../../assets/VerticalCards/SobreCards/Kess.svg';
+
 
 function Sobre(props){
     return(
@@ -10,16 +20,14 @@ function Sobre(props){
             <Container>
                 <h1>Quem somos?</h1>
                     <div>
+                <Space size="85px" sizeMobile="55px" />
+
                     <Presentation
                     width="480px"
                     src={krimps}
                     alt="Logo KRIMPS"
 
                 >
-
-                {/* <img src={krimps} alt="Logo KRIMPS"/> */}
-
-
 
                         <div id='msg'>
                             Somos um grupo de estudantes de engenharia de software da Universidade de Brasília do Campus
@@ -36,6 +44,45 @@ function Sobre(props){
 
                 </Presentation>
                     </div>
+                <Space size="85px" sizeMobile="10px" />
+
+                    <CardGroup>
+                        <Card vertical>
+                        <img alt="" src={Mat} />
+                        <a href="https://github.com/Matheus73" target="_blank" rel="noopener noreferrer">Matheus Gabriel A. Rodrigues</a>
+                        <p>CEO do grupo</p>
+                        </Card>
+
+                        <Card vertical>
+                        <img alt="" src={Ped} />
+                        <a href="https://github.com/Pedrok99" target="_blank" rel="noopener noreferrer">Pedro Henrique V. Lima</a>
+                        <p>Desenvolvedor Back-end</p>
+                        </Card>
+
+                        <Card vertical>
+                        <img alt="" src={Ig} />
+                        <a href="https://github.com/igorq937" target="_blank" rel="noopener noreferrer">Igor Queiroz Lima</a>
+                        <p>Desenvolvedor Front-end</p>
+                        </Card>
+
+                        <Card vertical>
+                        <img alt="" src={Sam} />
+                        <a href="https://github.com/SamuelNoB" target="_blank" rel="noopener noreferrer">Samuel Nogueira Bacelar</a>
+                        <p>Desenvolvedor Back-end</p>
+                        </Card>
+
+                        <Card vertical>
+                        <img alt="" src={Kess} />
+                        <a href="https://github.com/kessJhones" target="_blank" rel="noopener noreferrer">Kess Jhones</a>
+                        <p>Desenvolvedor Back-end</p>
+                        </Card>
+
+                        <Card vertical>
+                        <img alt="" src={Rob} />
+                        <a href="https://github.com/sayuck" target="_blank" rel="noopener noreferrer">Roberto Martins da Nóbrega</a>
+                        <p>Desenvolvedor Front-end</p>
+                        </Card>
+                    </CardGroup>
 
             </Container>
         </PageDefault>
--- front/src/pages/Sobre/styles.js
+++ front/src/pages/Sobre/styles.js
@@ -15,36 +15,9 @@ h1{
     text-align: center;
     padding: 15px 0;
 }
-/* img{
-    position: relative;
-    max-width: 480px;
-    border-radius: 20px;
-    padding: 20px;
+a{
+    color: #000000;
 }
-div{
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    text-align: center;
-    padding: 30px 15px;
-
-
-}
-#group{
-    flex-direction: column;
-    margin-top: 40px;
-}
-#msg{
-    border: 1px solid #000000;
-    box-sizing: border-box;
-    border-radius: 50px;
-} */
-
-
-
-/* @media screen and (max-width: 320px){
-    padding: 30px 20px;
-} */
 
 `;
 
