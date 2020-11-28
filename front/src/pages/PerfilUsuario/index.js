d27765a174839a4295893da01147fd29e05a5db2
matheus73 <matheuscoding@gmail.com>
2020-11-28 00:35:37 -0300

    Criando carrossel

--- front/src/pages/PerfilUsuario/index.js
+++ front/src/pages/PerfilUsuario/index.js
@@ -7,6 +7,9 @@ import CardGroup from '../../components/CardGroup';
 import imgProfileDefault from '../../assets/profileDefault.png';
 import axios from 'axios';
 
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 class PerfilUsuario extends Component {
     constructor(props){
         super(props);
@@ -20,10 +23,15 @@ class PerfilUsuario extends Component {
                 idUsuario: localStorage.getItem('idUsuario'),
                 descricao: localStorage.getItem('descricao'),
                 localidade: localStorage.getItem('localidade'),
-                descricao: localStorage.getItem('descricao'),
                 imagemPerfil: localStorage.getItem('imagemPerfil'),
                 categoria: localStorage.getItem('categoria')
-            }
+            },
+            //! Imagens de teste
+            //imgs: [] maneira oficial
+            imgs: [<img src="https://mapa-da-obra-producao.s3.amazonaws.com/wp-content/uploads/2018/09/assessoria-de-obras.jpg" width={1000} height={500} alt="Publicação"/>,
+            <img src="https://respostas.sebrae.com.br/wp-content/uploads/2019/09/386a2dfe736952d86d6f4f07387bc1d8.jpg"width={1000} height={500} alt="Publicação"/>,
+            <img src="https://www.institutodaconstrucao.com.br/blog/wp-content/uploads/2017/01/IMG-20151017-WA0016-2-810x425.jpg" width={1000} height={500} alt="Publicação"/>]
+
         }
     }
 
@@ -46,6 +54,18 @@ class PerfilUsuario extends Component {
                 this.setState({
                     posts: imagem,
                 })
+                
+                let imgs = []
+                for(i in response.data){
+                    let nomeImagem = response.data[i].nomeImagem;
+                    imgs.push(<img src={nomeImagem} width={1000} height={500} alt="Publicação"/>)
+                }
+                //! Para teste manter comentada
+                // this.setState({
+                //     imgs: imgs
+                // })
+
+                //? console.log(this.state.imgs)
             })
             .catch(error => {
                 console.log(error);
@@ -78,6 +98,10 @@ class PerfilUsuario extends Component {
                             {localStorage.getItem("localidade") || "Não definido"}</p>
                         </Card>
                     </CardGroup>
+                    <div id="carrossel">
+                        <h1>Publicações</h1>
+                        <AliceCarousel mouseTracking items={this.state.imgs}  infinite={true}/>
+                    </div>
                 </main>
                 <Footer/>
             </>
