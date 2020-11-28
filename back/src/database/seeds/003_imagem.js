e43edb9db7f4888d0aa79207ffc6a38a32694a02
Pedrok99 <Pedrohlima20@gmail.com>
2020-11-28 20:04:55 -0300

    trocando seed de imagens

--- back/src/database/seeds/003_imagem.js
+++ back/src/database/seeds/003_imagem.js
@@ -8,7 +8,8 @@ exports.seed = function(knex) {
       const imgs = []
       for (let index = 1; index <= numberOfPubs; index++) {
         imgs.push({
-          nomeImagem:'fakePubImage'+index+'.jpg',
+          nomeImagem:'dd1c16db-photo%20perfil.jpg',
+          url:'https://obracertaupload.s3.amazonaws.com/f9f02e7a-142f-4223-ac63-3987dd1c16db-photo perfil.jpg',
           idPublicacao : index
         });
 
