29e0d418e73f8bb16a85fa9ecb2cf7f1c4cb1242
Roberto Nóbrega <nobrega.m.roberto@gmail.com>
2020-11-29 15:53:39 -0300

    lixos

---
+++ front/node_modules/react-alice-carousel/lib/utils/classnames.d.ts
@@ -0,0 +1,4 @@
+import { State } from '../types';
+export declare const getRenderStageItemClasses: (i: number | undefined, state: State) => string;
+export declare const isActiveItem: (i: number | undefined, state: State) => boolean;
+export declare const isClonedItem: (i: number | undefined, state: State) => boolean;
