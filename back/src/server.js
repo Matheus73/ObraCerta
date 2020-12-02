b9719fba72c91eaf5a2eb719bd70f208e34468da
matheus73 <matheuscoding@gmail.com>
2020-12-02 08:19:15 -0300

    Preparando deploy front

--- back/src/server.js
+++
@@ -1,7 +0,0 @@
-const  app = require('./app');
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Running in ${process.env.MULTER_CONFIG}`)
-  console.log(`Listening to http://localhost:${PORT}`)
-})
