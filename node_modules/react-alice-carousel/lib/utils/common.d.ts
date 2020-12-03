8ae379d450875a568aefe8e6609de20953979a21
igorq937 <igorq937@gmail.com>
2020-12-02 22:43:52 -0300

    Retirando node_modules

--- node_modules/react-alice-carousel/lib/utils/common.d.ts
+++
@@ -1,5 +0,0 @@
-import { Props, State } from '../types';
-export declare const getIsStageContentPartial: (stageWidth?: number, contentWidth?: number) => boolean;
-export declare const getStageContentWidth: (state: Partial<State>) => number;
-export declare const getItemsInSlide: (itemsCount: number, props: Props) => number;
-export declare const calculateInitialState: (props: Partial<Props>, el: any) => State;
